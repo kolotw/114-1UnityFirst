ca87e8adec2f10fbb760388d5729ae205f31eacb
kolotw <kolo.tw@gmail.com>
2025-12-12 11:01:19 +0800

    1212-2

--- Assets/Scripts/NPC_RUN.cs
+++ Assets/Scripts/NPC_RUN.cs
@@ -13,6 +13,9 @@ public class NPC_RUN : MonoBehaviour
     public int 血量 = 100;
     int 原始血量;
     public Transform 血條;
+    bool 開始攻擊 = false;
+    float 攻擊間距 = 2f;
+    float 下次可攻擊時間;
     void Start()
     {
         導航 = GetComponent<NavMeshAgent>();
@@ -26,8 +29,24 @@ public class NPC_RUN : MonoBehaviour
         {
             導航.SetDestination(目標.position);
             距離 = Vector3.Distance(目標.position, this.transform.position);
-            if(距離 <= 3.1f)   { 動畫器.SetBool("isWalk",false); }
-            else                       { 動畫器.SetBool("isWalk", true); }
+            if(距離 <= 3.1f)   
+            { 
+                動畫器.SetBool("isWalk",false);
+                開始攻擊 = true;
+            }
+            else                       
+            { 
+                動畫器.SetBool("isWalk", true);
+                開始攻擊 = false;
+            }
+            if(開始攻擊)
+            {
+                if (Time.time >= 下次可攻擊時間)
+                {
+                    動畫器.SetTrigger("isAttack");
+                    下次可攻擊時間 = Time.time + 攻擊間距;
+                }
+            }
         }
     }
     private void OnTriggerEnter(Collider other)
@@ -39,7 +58,14 @@ public class NPC_RUN : MonoBehaviour
             血量文字.text = 血量.ToString();
             float 血量比例 = (float)血量 / (float)原始血量;            
             血條.localScale = new Vector3(血量比例,1,1);
-            if(血量 <= 0)  {  Destroy(this.gameObject);  }
+            if (血量 <= 0)
+            {
+                動畫器.SetTrigger("isDead");
+                Destroy(this.gameObject, 3f);
+            }
+            else {
+                動畫器.SetTrigger("isHit");
+            }
         }
     }
 }
