d002d18926d534476867ee37aab13d05b98c9b93
kolotw <77420523+kolotw@users.noreply.github.com>
2025-12-30 02:16:14 +0800

    1230

--- Assets/Scripts/NPC_RUN.cs
+++ Assets/Scripts/NPC_RUN.cs
@@ -1,10 +1,8 @@
 ﻿using UnityEngine;
-using UnityEngine.AI;
 using TMPro;
 
 public class NPC_RUN : MonoBehaviour
-{
-    private NavMeshAgent 導航;
+{    
     private Animator 動畫器;
     public Transform 目標;
     public float 距離=0;
@@ -14,49 +12,16 @@ public class NPC_RUN : MonoBehaviour
     public int 血量 = 100;
     int 原始血量;
     public Transform 血條;
-    bool 開始攻擊 = false;
-    float 攻擊間距 = 2f;
-    float 下次可攻擊時間;
     public float 攻擊距離 = 1.2f;
     void Start()
     {
-        導航 = GetComponent<NavMeshAgent>();
         動畫器 = GetComponent<Animator>();
         原始血量 = 血量;
         血量文字.text = 血量.ToString();
-        導航.stoppingDistance = 攻擊距離;
     }
     void Update()
     {
-        血條組件.transform.forward = Camera.main.transform.forward;
-        if (目標 != null)
-        {
-            導航.SetDestination(目標.position);
-            距離 = Vector3.Distance(目標.position, this.transform.position);
-            if(距離 <= 攻擊距離)   
-            { 
-                動畫器.SetBool("isWalk",false);
-                開始攻擊 = true;
-            }
-            else                       
-            { 
-                動畫器.SetBool("isWalk", true);
-                開始攻擊 = false;
-            }
-            if(開始攻擊)
-            {
-                if (血量 <= 0) return;
-                if (Time.time >= 下次可攻擊時間)
-                {
-                    動畫器.SetTrigger("isAttack");
-                    下次可攻擊時間 = Time.time + 攻擊間距;
-                }
-            }
-        }
-        else
-        {
-            目標 = GameObject.FindGameObjectWithTag("Player").transform;
-        }
+        血條組件.transform.forward = Camera.main.transform.forward;        
     }
     private void OnTriggerEnter(Collider other)
     {
